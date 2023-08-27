034d6673ac18798eb8c99b3d7ff790e9c7216101
nipulviduranga <e19413@eng.pdn.ac.lk>
2023-08-27 20:12:25 +0530

    Frontend

---
+++ Frontend/src/dto/RegisterUserDTO.js
@@ -0,0 +1,14 @@
+// dtos/userDTO.js
+export class RegisterUserDTO {
+    constructor(email, password, firstName,lastName,phoneNumber,addL1,addL2,addL3) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.addressL1 = addL1;
+        this.addressL2 = addL2;
+        this.addressL3 = addL3;
+        this.password = password;
+        this.img = '';
+    }
+}
