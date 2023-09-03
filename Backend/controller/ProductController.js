8608a3c187502607f4cd8260b9edfc8afef3d31b
Kaushalya193 <e19193@eng.pdn.ac.lk>
2023-09-03 16:21:39 +0530

    Create ProductController.js

---
+++ Backend/controller/ProductController.js
@@ -0,0 +1,44 @@
+// ProductController.js
+const db = require('../database');
+
+
+async function getTopSellingProducts(req, res) {
+  try {
+    const sql = 'SELECT * FROM product'; 
+    db.query(sql, (error, results) => {
+      if (error) {
+        console.error('Error fetching top-selling products from the database:', error);
+        res.status(500).json({ error: 'Database error' });
+      } else {
+        res.json(results); 
+      }
+    });
+  } catch (error) {
+    console.error('Error fetching top-selling products:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+
+async function getNewlyAddedProducts(req, res) {
+    try {
+      const sql = 'SELECT * FROM product ';
+      db.query(sql, (error, result) => {
+        if (error) {
+          console.error('Error fetching newly added products from the database:', error);
+          res.status(500).json({ error: 'Database error' });
+        } else {
+          res.json(result);
+        }
+      });
+    } catch (error) {
+      console.error('Error fetching newly added products:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+  
+
+module.exports = {
+  getTopSellingProducts,
+  getNewlyAddedProducts,
+};
