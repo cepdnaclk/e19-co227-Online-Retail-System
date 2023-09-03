a4973b4a85ccef747e951163c7894ce4a1545d9b
Kaushalya193 <e19193@eng.pdn.ac.lk>
2023-09-03 16:21:45 +0530

    Update systemRoutes.js

--- Backend/routes/systemRoutes.js
+++ Backend/routes/systemRoutes.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const customerController = require('../controller/AdminUserController');
+const productController = require('../controller/ProductController');
 
 router.post('/registerUser', customerController.createCustomer);
 router.post('/loginUser', customerController.LoginUser);
+router.get('/top-selling-products', productController.getTopSellingProducts); 
+router.get('/newly-added-products', productController.getNewlyAddedProducts); 
+
 
 module.exports = router;
