26f4c5e7a3e10bfadc6114592712e3a6c405993a
Kaushalya193 <e19193@eng.pdn.ac.lk>
2023-09-07 10:18:10 +0530

    Update ProductController.js

--- Backend/controller/ProductController.js
+++ Backend/controller/ProductController.js
@@ -22,7 +22,7 @@ async function getTopSellingProducts(req, res) {
 
 async function getNewlyAddedProducts(req, res) {
     try {
-      const sql = 'SELECT * FROM product ';
+      const sql = 'SELECT * FROM product ORDER BY productID DESC LIMIT 2;';
       db.query(sql, (error, result) => {
         if (error) {
           console.error('Error fetching newly added products from the database:', error);
