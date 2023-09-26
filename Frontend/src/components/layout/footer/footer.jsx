4c966bef11b3d2eedfd68e67e90e4b0333b13524
Kaushalya193 <e19193@eng.pdn.ac.lk>
2023-09-26 23:10:53 +0530

    Update footer.jsx

--- Frontend/src/components/layout/footer/footer.jsx
+++ Frontend/src/components/layout/footer/footer.jsx
@@ -14,89 +14,89 @@ import '../../../css/bootstrap-grid.css'
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="container-fluid bg-dark text-secondary mt-5 pt-5">
+      <div className="container-fluid bg-secondary text-white mt-5 pt-5">
   <div className="row px-xl-5 pt-5">
     <div className="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5 ">
-      <h5 className="text-secondary text-uppercase mb-4 text-white">Get In Touch</h5>
+      <h5 className="text-white text-uppercase mb-4 ">Get In Touch</h5>
       <p className="mb-4">
       Our company is dedicated to providing high-quality products and exceptional customer service. We take pride in our commitment to excellence and strive to meet the unique needs of each of our customers. With years of experience in the industry, we have established a strong reputation for reliability and innovation. We look forward to serving you and exceeding your expectations.
 
       </p>
       <p className="mb-2">
-        <i className="fa fa-map-marker-alt text-success mr-3" />
+        <i className="fa fa-map-marker-alt text-warning mr-3" />
         123 Street, Colombo 9, Sri Lanka 
       </p>
       <p className="mb-2">
-        <i className="fa fa-envelope text-success mr-3 yellow"  />
+        <i className="fa fa-envelope text-warning mr-3 yellow"  />
         info@example.com
       </p>
       <p className="mb-0">
-        <i className="fa fa-phone-alt text-success mr-3" />
+        <i className="fa fa-phone-alt text-warning mr-3" />
         +012 345 67890
       </p>
     </div>
     <div className="col-lg-8 col-md-12">
       <div className="row">
         <div className="col-md-4 mb-5">
-          <h5 className="text-secondary text-uppercase mb-4 text-white">Quick Shop</h5>
+          <h5 className="text-white text-uppercase mb-4 text-white">Quick Shop</h5>
           <div className="d-flex flex-column justify-content-start">
-            <a className="text-secondary mb-2" href="/">
+            <a className="text-white mb-2" href="/">
               <i className="fa fa-angle-right mr-2" />
               Home
             </a>
-            <a className="text-secondary mb-2" href="/products">
+            <a className="text-white mb-2" href="/products">
               <i className="fa fa-angle-right mr-2" />
               Our Shop
             </a>
-            <a className="text-secondary mb-2" href="#">
+            <a className="text-white mb-2" href="#">
               <i className="fa fa-angle-right mr-2" />
               Shop Detail
             </a>
-            <a className="text-secondary mb-2" href="#">
+            <a className="text-white mb-2" href="#">
               <i className="fa fa-angle-right mr-2" />
               Shopping Cart
             </a>
-            <a className="text-secondary mb-2" href="#">
+            <a className="text-white mb-2" href="#">
               <i className="fa fa-angle-right mr-2" />
               Checkout
             </a>
-            <a className="text-secondary" href="#">
+            <a className="text-white" href="#">
               <i className="fa fa-angle-right mr-2" />
               Contact Us
             </a>
           </div>
         </div>
         <div className="col-md-4 mb-5">
-          <h5 className="text-secondary text-uppercase mb-4 text-white">My Account</h5>
+          <h5 className="text-white text-uppercase mb-4 text-white">My Account</h5>
           <div className="d-flex flex-column justify-content-start">
-            <a className="text-secondary mb-2" href="/">
+            <a className="text-white mb-2" href="/">
               <i className="fa fa-angle-right mr-2" />
               Home
             </a>
-            <a className="text-secondary mb-2" href="/products">
+            <a className="text-white mb-2" href="/products">
               <i className="fa fa-angle-right mr-2" />
               Our Shop
             </a>
-            <a className="text-secondary mb-2" href="#">
+            <a className="text-white mb-2" href="#">
               <i className="fa fa-angle-right mr-2" />
               Shop Detail
             </a>
-            <a className="text-secondary mb-2" href="#">
+            <a className="text-white mb-2" href="#">
               <i className="fa fa-angle-right mr-2" />
               Shopping Cart
             </a>
-            <a className="text-secondary mb-2" href="#">
+            <a className="text-white mb-2" href="#">
               <i className="fa fa-angle-right mr-2" />
               Checkout
             </a>
-            <a className="text-secondary" href="#">
+            <a className="text-white" href="#">
               <i className="fa fa-angle-right mr-2" />
               Contact Us
             </a>
           </div>
         </div>
         <div className="col-md-4 mb-5">
-          <h5 className="text-secondary text-uppercase mb-4 text-white">Newsletter</h5>
+          <h5 className="text-white text-uppercase mb-4 text-white">Newsletter</h5>
           <p>Subscribe to our newsletter for the latest updates, exclusive offers, and news about our products and services.</p>
           <form action="">
             <div className="input-group">
@@ -106,22 +106,22 @@ const Footer = () => {
                 placeholder="Your Email Address"
               />
               <div className="input-group-append">
-                <button className="btn btn-success text-dark">Sign Up</button>
+                <button className="btn btn-warning text-dark">Sign Up</button>
               </div>
             </div>
           </form>
-          <h6 className="text-secondary text-uppercase mt-4 mb-3 text-white">Follow Us</h6>
+          <h6 className="text-white text-uppercase mt-4 mb-3 text-white">Follow Us</h6>
           <div className="d-flex">
-            <a className="btn btn-success btn-square mr-2 " href="#">
+            <a className="btn btn-warning btn-square mr-2 " href="#">
               <i className="fab fa-twitter text-dark" />
             </a>
-            <a className="btn btn-success btn-square mr-2" href="#">
+            <a className="btn btn-warning btn-square mr-2" href="#">
               <i className="fab fa-facebook-f text-dark" />
             </a>
-            <a className="btn btn-success btn-square mr-2"  href="#">
+            <a className="btn btn-warning btn-square mr-2"  href="#">
               <i className="fab fa-linkedin-in text-dark" />
             </a>
-            <a className="btn btn-success btn-square" href="#">
+            <a className="btn btn-warning btn-square" href="#">
               <i className="fab fa-instagram text-dark" />
             </a>
           </div>
@@ -134,13 +134,13 @@ const Footer = () => {
     style={{ borderColor: "rgba(256, 256, 256, .1) !important" }}
   >
     <div className="col-md-6 px-xl-0">
-      <p className="mb-md-0 text-center text-md-left text-secondary">
+      <p className="mb-md-0 text-center text-md-left text-white">
         ©{" "}
-        <a className="text-success text-#ff377" href="#">
+        <a className="text-warning text-#ff377" href="#">
           Domain
         </a>
         . All Rights Reserved. Designed by
-        <a className="text-success" href="https://htmlcodex.com">
+        <a className="text-warning" href="https://htmlcodex.com">
           HTML Codex
         </a>
       </p>
