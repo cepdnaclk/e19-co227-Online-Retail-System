2741f5d10c2e6ca41b3bc29d542c52b770b07646
SupunThilanka <115542268+SupunThilanka@users.noreply.github.com>
2023-10-16 20:03:03 +0530

    some changes on success page

--- Frontend/src/pages/Checkout/Success.js
+++ Frontend/src/pages/Checkout/Success.js
@@ -7,7 +7,7 @@ function successful(){
     return(
 
         <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-        <h2 style={{ textAlign: "center" }}>Thank You! Your Order has been Successfully placed.</h2>
+        <h2 style={{ textAlign: "center" }}>Thank You! Your Order has been Successfully Placed.</h2>
         <img style={{ height: "250px", marginTop: "50px" }} src={successImg} />
         <h4 style={{ marginTop: "50px" }}>
             <NavLink to="/" style={{ textDecoration: 'none', backgroundColor: "#3CD2A2", padding: "10px 20px", borderRadius: "5px", color: "black" }}>Shop More</NavLink>
